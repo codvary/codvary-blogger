1ec834acd82d4ff06cadbd6314de5281900f6cd6
codvary <codvary@gmail.com>
2024-12-15 13:28:01 +0530

    adding user list left pannel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './component/header/header.component';
+import { UserComponent } from "./component/user/user.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
