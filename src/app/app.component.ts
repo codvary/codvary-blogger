b3370c25012c720758c16f83c2525d4d1cbf7d4e
codvary <codvary@gmail.com>
2024-12-15 12:34:20 +0530

    added header component and create user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './component/header/header.component';
-import { BloggerComponent } from "./component/blogger/blogger.component";
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, BloggerComponent],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blogger home';
+
 }
